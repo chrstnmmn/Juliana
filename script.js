71de39b9573ef4aacbbff7de5cdad9d89967c397
Christian Emmanuel <christianemmanuelcollo23@gmail.com>
2024-06-09 18:35:49 +0800

    Add files via upload
    added new feature:
    - allow to view which table correspond to guest
    - uninvited guest are not allowed to enter

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
+//always move scroll the page back to top when the page refresh
+const targetY = document.getElementsByClassName("Flowers1");
 window.onload = function () {
-  window.scrollTo(0, 0);
+  window.scrollTo({ top: targetY, behavior: "smooth" });
 };
 
-const guestList = {
-  Margaret: 1,
-  Juliana: 2,
-  Emman: 3,
-  Raica: 4,
-  // Add more guests as needed
-};
-
+//when the button is clicked, the website will check if it's invited or not
 function checkName() {
   const name = document.getElementById("userName").value.trim();
+  const outputName = document.getElementById("userNameText");
+  const tableNumber = document.getElementById("seatNumberText");
+  const userInput = document.getElementById("userName");
   //const result = document.getElementById("result");
 
   if (guestList[name]) {
     document.body.style.overflowY = "scroll";
-    alert("Horay you are invited!");
+    const targetY = document.getElementById("resultPart").offsetTop;
+    window.scrollTo({
+      top: targetY,
+      behavior: "smooth",
+    });
+
+    userInput.value = "";
+    outputName.innerHTML = name;
+    tableNumber.innerHTML = guestList[name];
   } else {
-    document.body.style.overflow = "hidden";
-    alert("You are not invited");
+    window.scrollTo({ top: targetY, behavior: "smooth" });
+    userInput.value = "";
+    userInput.placeholder = "Sorry, you are not invited.";
     setTimeout(() => {
-      window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0"; // Change to the actual URL you want to redirect to
-    }, 200); // Redirect after .2 seconds
+      document.body.style.overflowY = "hidden";
+    }, 300); // Redirect after 1 second(s)
   }
 }
+
+//lists of names that is invited
+const guestList = {
+  "Margaret Julianne Juan": 11,
+  "Juliana Kim Borbe": 6,
+  "Christian Emmanuel Collo": 23,
+  "Jay - jay Dawal": 5,
+  "Lemuel Clemente": 4,
+  "Alex Poblete": 6,
+  // Add more guests as needed
+};
